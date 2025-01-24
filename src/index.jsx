37f52787c088e08a74a24e5c58d33a5f1ca3f27b
francelino <fmarcilio1989@gmail.com>
2025-01-24 07:52:34 -0300

    Feat: Header da aplicação

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import { Header } from "./components/Header";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <p>Bem vindo!</p>
+    </>
+  );
+}
+
+export default App;
