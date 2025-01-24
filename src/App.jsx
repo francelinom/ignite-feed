fd27bc789d355a4d39d396d250a19f5383ec945f
francelino <fmarcilio1989@gmail.com>
2025-01-23 21:32:16 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Header } from './components/Header'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+    <Header></Header>
+      <p>Bem vindo!</p>
+    </>
+  )
+}
+
+export default App
