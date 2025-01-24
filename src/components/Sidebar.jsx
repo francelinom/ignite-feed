b8af9d6de83f3fe43ec1959fcf79db53bb19e2c0
francelino <fmarcilio1989@gmail.com>
2025-01-24 09:05:21 -0300

    Feat: styles de sidebar e adicionando biblioteca de icons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,20 +1,29 @@
-import style from "./Sidebar.module.css";
+import { PencilLine } from "phosphor-react";
+
+import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
   return (
-    <aside className={style.sidebar}>
+    <aside className={styles.sidebar}>
       <img
-        className={style.cover}
+        className={styles.cover}
         src="https://images.unsplash.com/photo-1511376777868-611b54f68947?q=80&w=1470&auto=format&fit=crop&w=500&q=50&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       />
 
-      <div>
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/francelinom.png"
+        />
         <strong>Francelino Marcílio</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
-        <a href="3">Editar seu perfil</a>
+        <a href="3">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
