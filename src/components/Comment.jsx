ed05a76c8e7e2f514915d766939163f0f69d586b
francelino <fmarcilio1989@gmail.com>
2025-01-24 10:44:36 -0300

    Feat: styles de comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -9,13 +9,13 @@ export function Comment() {
         <div className={styles.commentContent}>
           <header>
             <div className={styles.authorAndTime}>
-              <strong>Diego Fernandes</strong>
+              <strong>Francelino Marcílio</strong>
               <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">
                 Cerca de 1h atrás
               </time>
             </div>
             <button title="Deletar comentário">
-              <Trash size={20} />
+              <Trash size={24} />
             </button>
           </header>
           <p>Muito bom Devon, parabéns!! 👏👏</p>
