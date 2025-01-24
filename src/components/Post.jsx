a02eafd1a908373c05abd6ff87af38bd50dfe4e9
francelino <fmarcilio1989@gmail.com>
2025-01-24 10:37:05 -0300

    Feat: estrutura component comment

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
 export function Post() {
@@ -36,6 +37,20 @@ export function Post() {
           <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
           <a href="">#rocketseat</a>
         </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Deixe um comentário"></textarea>
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        <Comment />
       </div>
     </article>
   );
